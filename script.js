2d3e1ea1afc0124bdb97a0d5437bcb52fd37962b
simulasijude-lab <simulasijude@gmail.com>
2026-05-26 17:11:07 +0700

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 // ⚠️ GANTI DENGAN PUNYA ANDA
-const SUPABASE_URL = 'https://iadrrnkztxdwqubrnbqp.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://iadrrnkztxdwqubrnbqp.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhZHJybmt6dHhkd3F1YnJuYnFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3ODAwNzYsImV4cCI6MjA5NTM1NjA3Nn0.Zrku8OpcbUnJ0kaG0ibWQ-oCK5pKia290e15ZNSZDi4';
 
 // Inisialisasi Supabase Client
